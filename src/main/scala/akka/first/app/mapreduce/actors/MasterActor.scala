2e58240846eee172b557161f6333c7dacc6b6581
Erdong Feng <edfeng@thoughtworks.com>
2014-06-22 09:38:04 +0800

    use 5 instances of mapActor.

--- src/main/scala/akka/first/app/mapreduce/actors/MasterActor.scala
+++ src/main/scala/akka/first/app/mapreduce/actors/MasterActor.scala
@@ -2,11 +2,12 @@ package akka.first.app.mapreduce.actors
 
 import akka.actor.{Actor, Props}
 import akka.first.app.mapreduce.Result
+import akka.routing.{RoundRobinPool, RoundRobinRouter}
 
 class MasterActor extends Actor {
   val aggregateActor = context.actorOf(Props[AggregateActor], "aggregate")
   val reduceActor = context.actorOf(Props(new ReduceActor(aggregateActor)), "reduce")
-  val mapActor = context.actorOf(Props(new MapActor(reduceActor)), "map")
+  val mapActor = context.actorOf(Props(new MapActor(reduceActor)).withRouter(RoundRobinPool(nrOfInstances = 5)), "map")
 
   override def receive: Receive = {
     case msg: String => mapActor ! msg
